ce609f52e1adc6396c4087358cc80db19dceb2b7
evolk <ekavolkvolk@gmail.com>
2022-05-08 16:00:12 +0200

    create_note_range

--- source_code/ear_training_game/ear_training_game/create_MuseScore_file.py
+++ source_code/ear_training_game/ear_training_game/create_MuseScore_file.py
@@ -1,6 +1,21 @@
-from xml.etree import cElementTree as ET
-from collections import defaultdict
-import random
-import numpy as np
+# from xml.etree import cElementTree as ET
+# from collections import defaultdict
+# import random
+# import numpy as np
+# import string
 
 
+def create_note_range():
+    full_range = []
+    octaves = list(range(2,6))
+    notes = ["C", "D", "E", "F", "G", "A", "B"]
+    
+    for o in octaves:
+        for n in notes:
+            full_range.append((n,o))
+
+    full_range.append(("C", 6))
+    return full_range
+    
+
+print (create_note_range())
